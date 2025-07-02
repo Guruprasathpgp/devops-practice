ba95f4a0bb9aa3b36e5294d41c9d1ee35579f668
root <root@ip-172-31-88-185.ec2.internal>
2025-07-02 06:16:57 +0000

    webhook testing

--- src/main/java/com/example/CompanyServlet.java
+++ src/main/java/com/example/CompanyServlet.java
@@ -12,12 +12,12 @@ public class CompanyServlet extends HttpServlet {
 
         out.println("<!DOCTYPE html>");
         out.println("<html><head>");
-        out.println("<title>About Us - GP Corp</title>");
+        out.println("<title>About Us - Guruprasath</title>");
         out.println("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet'>");
         out.println("</head><body class='bg-white'>");
 
         out.println("<div class='container mt-5'>");
-        out.println("<h1 class='text-center text-dark'>About GP Corporation</h1>");
+        out.println("<h1 class='text-center text-dark'>About Guruprasath Corporation</h1>");
         out.println("<p class='lead text-center'>We build innovative software solutions for businesses across the globe.</p>");
         out.println("<div class='text-center'><a href='/java-webapp/' class='btn btn-secondary'>Back to Home</a></div>");
         out.println("</div>");
